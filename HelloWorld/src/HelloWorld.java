8d84987ab673c569545fea99b17c26568fab36bb
raquelprofeinfo <raquelprofeinfo@users.noreply.github.com>
2013-10-31 19:07:39 +0100

    Calculadora científica

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,8 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("SÛc una calculadora simple");
+		System.out.println("Calculadora cient√≠fica");
 	}
 
 }
