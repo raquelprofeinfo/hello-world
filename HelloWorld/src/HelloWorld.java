6cec7088426e0d827f8e92bd99525ebff5c02d0b
raquelprofeinfo <raquelprofeinfo@users.noreply.github.com>
2013-10-31 18:32:22 +0100

    Modificacio cientifica

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,8 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("S�c una calculadora simple");
+		System.out.println("Sóc una calculadora científica");
 	}
 
 }
