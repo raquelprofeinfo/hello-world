7899154eb81ed10f13d71eba5b561ed7d6043b7c
raquelprofeinfo <raquelprofeinfo@users.noreply.github.com>
2013-10-31 19:18:43 +0100

    simple 3

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,11 +1,12 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sóc una calculadora simple");
-		System.out.println("Sóc una calculadora simple");
-		System.out.println("Sóc una calculadora simple");
+		System.out.println("Sï¿½c una calculadora simple");
+		System.out.println("Sï¿½c una calculadora simple");
+		System.out.println("Sï¿½c una calculadora simple");
+		System.out.println("Sï¿½c una calculadora simple");
+
 	}
 
 }
